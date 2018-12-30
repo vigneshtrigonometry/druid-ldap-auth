2199311b6b8f849f4f05013e8177b69be3c7e5e4
Vignesh <Vignesh@DESKTOP-HGHMBCR>
2018-12-30 23:12:03 +0800

    Initial commit with project structure

---
+++ src/main/java/com/vig/druid/ldap/authenticator/DruidLdapAuthenticator.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 Vignesh.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vig.druid.ldap.authenticator;
+
+import io.druid.server.security.AuthenticationResult;
+import io.druid.server.security.Authenticator;
+import java.util.EnumSet;
+import java.util.Map;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.client.api.Request;
+
+/**
+ *
+ * @author Vignesh
+ */
+public class DruidLdapAuthenticator implements Authenticator {
+
+    @Override
+    public Filter getFilter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getAuthChallengeHeader() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AuthenticationResult authenticateJDBCContext(Map<String, Object> map) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void decorateProxyRequest(HttpServletRequest hsr, HttpServletResponse hsr1, Request rqst) {
+        Authenticator.super.decorateProxyRequest(hsr, hsr1, rqst); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Class<? extends Filter> getFilterClass() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<String, String> getInitParameters() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public EnumSet<DispatcherType> getDispatcherType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
